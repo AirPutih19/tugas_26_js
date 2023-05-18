a968d0769e67a6df18b9883a116896318f2e66e2
AirPutih19 <kharismadwisj@gmail.com>
2023-05-19 06:40:36 +0700

    Tugas 26 JS
    Tugas 26 JS untuk niomic

---
+++ tugas26.js
@@ -0,0 +1,17 @@
+var x = ([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16 ]);
+function panggilHasil(x){
+
+    if(isFinite(100/x)){
+        return " NOT Infinity."
+    }
+    return " Infinity"
+    
+}
+for(i=0;i<x.length;i++){
+    console.log("Angka",x[i],panggilHasil(x[i]))
+}
+
+    
+    
+   
+
